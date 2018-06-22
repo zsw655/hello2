c255121010a50352247eba1ff87e62746bedfac8
zsw655 <215095167@qq.com>
2018-06-22 13:54:15 +0800

    hello2 的maven工程

---
+++ util/src/main/java/com/zsw/Main.java
@@ -0,0 +1,7 @@
+package com.zsw;
+
+public class Main {
+    public static void main(String[] args){
+        MainUtil.fun();
+    }
+}
---
+++ util/src/main/java/com/zsw/MainUtil.java
@@ -0,0 +1,9 @@
+package com.zsw;
+
+public class MainUtil {
+
+    public static void fun(){
+        System.out.println("abc");
+    }
+
+}
